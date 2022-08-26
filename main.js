ed97fc15e674d5484b64fb20d05e5d9db1c2917e
Mazin Fouad <m.fouad@live.de>
2022-08-26 19:38:37 +0200

    render first letter from each land

--- main.js
+++ main.js
@@ -1,4 +1,5 @@
 let bundeslaender = [];
+let letters = [];
 
 async function init() {
   let response = await fetch('./bundesland.json');
@@ -13,12 +14,34 @@ function render() {
   for (let i = 0; i < bundeslaender.length; i++) {
     const land = bundeslaender[i];
     const population = (land.population + '').replace('.', ','); //string connected, replace methode works only with string
-
-    content.innerHTML += /*html*/ `
-    <a class= "land-box" href="${land.url} " target="_blank"> 
-      <div>${land.name}</div>
-      <span>${population} Millionen</span>
-    </a>
-    `;
+    content.innerHTML += createBoxLink(land, population);
+    const firstLetter = land['name'].charAt(0);
+    console.log(firstLetter);
+    if (!letters.includes(firstLetter)) {
+      letters.push(firstLetter);
+    }
   }
+
+  renderLetters();
+}
+
+  function renderLetters(){
+    let lettersContainer = document.getElementById('letters_container');
+    lettersContainer.innerHTML = '';
+    for (let i = 0; i < letters.length; i++) {
+      const letter = letters[i];
+      lettersContainer.innerHTML += /*html*/ `
+      <div class="letter-box">${letter}</div>
+      `;
+    }
+  }
+
+
+function createBoxLink(land, population) {
+  return /*html*/ `
+  <a class= "land-box" href="${land.url} " target="_blank"> 
+    <div>${land.name}</div>
+    <span>${population} Millionen</span>
+  </a>
+  `;
 }
